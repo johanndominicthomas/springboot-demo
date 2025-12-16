38adc511377e9806c5d809cbf77c5673cb0f19c4
johanndominicthomas <johannthomaspala@gmail.com>
2025-12-16 21:47:19 +0530

    Intial folder structure

---
+++ src/main/java/com/luminar/SpringbootMysqlJspTeacherSelfApplication.java
@@ -0,0 +1,13 @@
+package com.luminar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootMysqlJspTeacherSelfApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootMysqlJspTeacherSelfApplication.class, args);
+	}
+
+}
