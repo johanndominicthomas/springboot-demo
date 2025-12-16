2bc27a6c737d15a8145394a1b8f29b601ac2b226
johanndominicthomas <johannthomaspala@gmail.com>
2025-12-17 00:37:32 +0530

    Added get method for root mapping / in controller class

--- src/main/java/com/luminar/controller/TeacherController.java
+++ src/main/java/com/luminar/controller/TeacherController.java
@@ -1,8 +1,31 @@
 package com.luminar.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.luminar.entity.Teacher;
+import com.luminar.service.TeacherService;
 
 @Controller
 public class TeacherController {
 
+		@Autowired
+		TeacherService teacherService;
+		
+		@GetMapping("/")
+		public ModelAndView getAllTeachers() {
+			ModelAndView mav = new ModelAndView("index");
+			List<Teacher> teacherList = teacherService.getAllTeachers();
+			mav.addObject("teachers", teacherList);
+			return mav;
+		}
+		/*ModelAndView is internally same as  Model + String.
+		 * In Model, we set model with object to send to view and return jsp file name String.
+		 * In Model and View ,it holds both the jsp file name and the object to sent.
+		 * MAV is older SpringBoot.Model+ String is the newer one and reduces code.
+		 */
 }
